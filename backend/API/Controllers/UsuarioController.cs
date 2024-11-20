0574ba3f6e4eb633eb90c1fef1172b8680a79740
juanrey <juanrey@deloitte.com>
2024-11-20 17:19:47 -0300

    backend | Registro de usuarios creando hashes

--- backend/API/Controllers/UsuarioController.cs
+++ backend/API/Controllers/UsuarioController.cs
@@ -1,14 +1,14 @@
 ﻿using Data;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Models.DTOs;
 using Models.Entities;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UsuarioController : ControllerBase
+    public class UsuarioController : BaseApiController
     {
         private readonly ApplicationDbContext _db;
 
@@ -29,6 +29,29 @@ namespace API.Controllers
         {
             var usuario = await _db.Usuarios.FindAsync(id);
             return Ok(usuario);
+        }
+
+        [HttpPost("registro")]
+        public async Task<ActionResult<Usuario>> Registro(RegistroDto registroDto)
+        {
+            if (await UsuarioExiste(registroDto.Username)) return BadRequest("Ese usuario ya se encuentra registrado");
+            
+            using var hmac = new HMACSHA512();
+
+            var usuario = new Usuario
+            {
+                Username = registroDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registroDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+            _db.Usuarios.Add(usuario);
+            await _db.SaveChangesAsync();
+            return Ok(usuario);
+        }
+
+        private async Task<bool> UsuarioExiste( string username )
+        {
+            return await _db.Usuarios.AnyAsync(x => x.Username == username.ToLower());
         }
     }
 }
---
+++ backend/Models/DTOs/RegistroDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models.DTOs
+{
+    public class RegistroDto
+    {
+        [Required(ErrorMessage = "Username es Requerido")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Password es Requerido")]
+        public string Password { get; set; }
+    }
+}
