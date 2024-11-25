ed4afc3579143ac1f72ccd7d7203a38ba56509c3
juanrey <juanrey@deloitte.com>
2024-11-24 23:17:48 -0300

    backend | Se agrega metodos de extension para mejorar la claridad del codigo

---
+++ backend/API/Extensiones/ServicioAplicacionExtension.cs
@@ -0,0 +1,51 @@
+﻿using Data.interfaces;
+using Data.services;
+using Data;
+using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensiones
+{
+    public static class ServicioAplicacionExtension
+    {
+        public static IServiceCollection AgregarServiciosAplicacion(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Ingresar Bearer [espacio] token \r\n\r\n Ejemplo Bearer ejoy98989",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "Bearer",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
+                        },
+                        new List<string>() // Una lista vacía de scopes ya que no es OAuth2
+                    }
+                });
+            });
+
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+            services.AddCors();
+            services.AddScoped<ITokenService, TokenService>();
+            return services;
+        }
+    }
+}
--- backend/API/Program.cs
+++ backend/API/Program.cs
@@ -1,37 +1,19 @@
+using API.Extensiones;
 using Data;
 using Data.interfaces;
 using Data.services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
-builder.Services.AddCors();
-builder.Services.AddScoped<ITokenService, TokenService>();
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])),
-            ValidateIssuer = false,
-            ValidateAudience = false,
-        };
-    });
-
+builder.Services.AgregarServiciosAplicacion(builder.Configuration);
+builder.Services.AgregarServicioIdentidad(builder.Configuration);
 
 var app = builder.Build();
 
