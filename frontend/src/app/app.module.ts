2edfa561cd790524545f11567142a3924ec12109
juanrey <juanrey@deloitte.com>
2024-11-28 12:12:38 -0300

    frontend | update packages

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,19 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserModule } from './users/users.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     BrowserAnimationsModule,
-    UserModule,
+    UserModule
   ],
-  bootstrap: [AppComponent]
+  /* providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ], */
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
